cb2829cbf3e5826102029d534d644b03b5b80a27
Nishva <fathimanishva7@gmail.com>
2026-08-01 21:19:50 +0530

    Completed authentication UI with login and register pages

--- static/js/script.js
+++ static/js/script.js
@@ -1,1 +1,21 @@
-console.log("GlowGuide is running!");
+function setupPasswordToggle(inputId, toggleId) {
+    const password = document.getElementById(inputId);
+    const toggle = document.getElementById(toggleId);
+
+    if (password && toggle) {
+        toggle.addEventListener("click", function () {
+
+            if (password.type === "password") {
+                password.type = "text";
+                this.innerHTML = '<i class="bi bi-eye-slash"></i>';
+            } else {
+                password.type = "password";
+                this.innerHTML = '<i class="bi bi-eye"></i>';
+            }
+
+        });
+    }
+}
+
+setupPasswordToggle("password", "togglePassword");
+setupPasswordToggle("confirmPassword", "toggleConfirmPassword");
